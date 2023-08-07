4ac22159c83324edce8c292d1173e56df6747f51
Meric Arda Eren <aeren@netas.com.tr>
2023-08-07 17:20:35 +0300

    initialize a simple react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+const App = ():ReactElement => {
+    return (
+        <>
+            <div> {process.env.REACT_APP_PORT}</div>
+        </>
+    )
+}
+
+export default App;
