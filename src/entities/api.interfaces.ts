9601f398eea9c2fbfccc90a3699a73bc8c506b6b
Meric Arda Eren <aeren@netas.com.tr>
2023-11-13 18:49:49 +0300

    Remove unused api interfaces

--- src/entities/api.interfaces.ts
+++ src/entities/api.interfaces.ts
@@ -1,23 +1,3 @@
-export interface FindAll<T> {
-  data: T[]
-}
-
-export interface Update<T> {
-  data: T
-}
-
-export interface Delete<T> {
-  data: T
-}
-
-export interface Create<T> {
-  data: T
-}
-
-export interface FindOne<T> {
-  data: T
-}
-
 export interface PaginationRequest {
   page: number
   limit: number
