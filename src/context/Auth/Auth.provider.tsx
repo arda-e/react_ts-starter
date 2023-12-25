815723975904fd679f6e6a018f432f004f8a0b08
ardaeren <76147924+arda-e@users.noreply.github.com>
2023-12-25 20:57:17 +0300

    Update Auth.provider.tsx

--- src/context/Auth/Auth.provider.tsx
+++ src/context/Auth/Auth.provider.tsx
@@ -1,22 +1,19 @@
-import React, { ReactElement, ReactNode, createContext, useReducer } from 'react'
-import { authReducer } from './Auth.reducers'
-import { AuthAction, AuthState } from './Auth.types'
+import React, { ReactElement, ReactNode, createContext, useReducer } from 'react';
+import { authReducer } from './Auth.reducers';
+import { AuthAction, AuthState } from './Auth.types';
 
-const initialState = {
+const initialState: AuthState = {
   isAuthenticated: false,
   user: null,
-}
+};
 
-export const AuthContext = createContext<
-  | {
-      state: AuthState
-      dispatch: React.Dispatch<AuthAction>
-    }
-  | undefined
->(undefined)
+export const AuthContext = createContext<{
+  state: AuthState;
+  dispatch: React.Dispatch<AuthAction>;
+} | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }): ReactElement => {
-  const [state, dispatch] = useReducer(authReducer, initialState)
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
-  return <AuthContext.Provider value={{ state, dispatch }}>{children}</AuthContext.Provider>
-}
+  return <AuthContext.Provider value={{ state, dispatch }}>{children}</AuthContext.Provider>;
+};
